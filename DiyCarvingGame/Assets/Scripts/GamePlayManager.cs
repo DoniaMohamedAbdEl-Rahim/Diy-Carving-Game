66f564ed070703fee631a7c6d3fdad7bf76e77dd
DoniaMohamed17 <doniamohamedabdelrahim@gmail.com>
2021-08-27 07:46:09 +0200

    cleaning code

--- DiyCarvingGame/Assets/Scripts/GamePlayManager.cs
+++ DiyCarvingGame/Assets/Scripts/GamePlayManager.cs
@@ -14,23 +14,23 @@ public class GamePlayManager : MonoBehaviour
     [SerializeField]
     GameObjectSO finalGameObject;
     bool done = false;
-    bool done2 = false;
+    bool changePosition = false;
     float speed =40f;
     private void Update()
     {
         if (done)
         {
             {
-                if (!done2)
+                if (!changePosition)
                     finalGameObject._gameObject.transform.position += new Vector3(0, 0, -20);
-                done2 = true;
+                changePosition = true;
             }
             finalGameObject._gameObject.transform.Rotate(speed * Time.deltaTime, speed * Time.deltaTime, speed * Time.deltaTime);
         }
     }
     public void Back_Clicked()
     {
-        Debug.Log("Clicked");
+        //Debug.Log("Clicked");
         SceneManager.LoadScene(0);
     }
     public void RestartGame()
@@ -40,12 +40,12 @@ public class GamePlayManager : MonoBehaviour
     public void Done()
     {
         done = true;
+        knife.SetActive(false);
         StartCoroutine(Animate());
     }
     IEnumerator Animate()
     {
         yield return new WaitForSeconds(5.0f);
-        finalGameObject._gameObject.transform.position = new Vector3(18, 9 , 10);
         gamePlayCanvas.gameObject.SetActive(false);
         lastCanvas.gameObject.SetActive(true);
     }
